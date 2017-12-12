3709a4a094967c6be8944abd369ecc8788e26951
myrto.koukouli <myrto.koukouli@d315682c-612b-4755-9ff5-7f18f6832af3>
2017-12-12 14:31:17 +0000

    remaining files from previous commit!

---
+++ dnet45/modules/uoa-repository-dashboard-gui/trunk/app/pages/sources/sources-register/sources-register-literature.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component ({
+  selector:'app-search-register-literature',
+  templateUrl: 'sources-register-literature.component.html'
+})
+
+export class SourcesRegisterLiteratureComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
