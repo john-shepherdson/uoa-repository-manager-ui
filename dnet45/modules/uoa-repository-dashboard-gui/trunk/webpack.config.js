3881ae0cbf64b9f9f944d0a0348e16a42c619668
stefania.martziou <stefania.martziou@d315682c-612b-4755-9ff5-7f18f6832af3>
2018-10-04 23:59:06 +0000

    Applied new theme

--- dnet45/modules/uoa-repository-dashboard-gui/trunk/webpack.config.js
+++ dnet45/modules/uoa-repository-dashboard-gui/trunk/webpack.config.js
@@ -77,7 +77,7 @@ var defaultConfig = {
     },
     proxy: {
       '/uoa-repository-manager-service': {
-        target: 'http://koulis.athenarc.gr:8480',
+        target: 'http://localhost:8480',
         secure: false
       }
     }
@@ -107,7 +107,7 @@ module.exports = function (env) {
           FAQ_ENDPOINT: JSON.stringify(process.env.FAQ_ENDPOINT || "https:/demo.openminted.eu/provide/api"),
           AAI_ENDPOINT: JSON.stringify(process.env.AAI_ENDPOINT || "/uoa-repository-manager-service/openid_connect_login"),
           FAQ_HOMEPAGE: JSON.stringify(process.env.FAQ_HOMEPAGE || "http://audrey.athenarc.gr:5555"),
-          BASE_URL: JSON.stringify(process.env.BASE_URL || "http://koulis.athenarc.gr:4200")
+          BASE_URL: JSON.stringify(process.env.BASE_URL || "http://localhost:4200")
         }
       })
     );
@@ -137,7 +137,7 @@ module.exports = function (env) {
           FAQ_ENDPOINT: JSON.stringify("http://audrey.athenarc.gr:5555/api"),
           AAI_ENDPOINT: JSON.stringify("/uoa-repository-manager-service/openid_connect_login"),
           FAQ_HOMEPAGE: JSON.stringify("http://audrey.athenarc.gr:5555"),
-          BASE_URL: JSON.stringify("http://koulis.athenarc.gr:4200")
+          BASE_URL: JSON.stringify("http://localhost:4200")
         }
       })
     );
