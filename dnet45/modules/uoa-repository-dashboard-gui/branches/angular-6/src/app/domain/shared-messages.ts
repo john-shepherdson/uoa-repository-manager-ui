01a0021aba4dc11a5cc5a14ad1367efac4a6d55a
myrto.koukouli <myrto.koukouli@d315682c-612b-4755-9ff5-7f18f6832af3>
2018-12-21 11:26:43 +0000

    uploading new ui

---
+++ dnet45/modules/uoa-repository-dashboard-gui/branches/angular-6/src/app/domain/shared-messages.ts
@@ -0,0 +1,91 @@
+
+/* Service Down Message */
+export const noServiceMessage = 'The service is not available at the moment.';
+
+/* Repositories Messages */
+export const loadingUserRepoInfoError = 'Failed to load information on your registered repositories';
+export const loadingUserRepoInfoEmpty = 'You have not yet registered any repositories';
+export const reposRetrievalError = 'The repositories can not be retrieved at the moment';
+export const noRepositoriesFound = 'No Datasources were found';
+export const loadingReposMessage = 'Retrieving repositories ...';
+export const noRepositoryChosenMsg = 'You need to select a repository first.';
+export const loadingRepoMessage = 'Retrieving repository information ...';
+export const loadingRepoError = 'System error retrieving repository info';
+export const loadingInterfacesError = 'System error retrieving interfaces';
+export const loadingAggregationHistory = 'Retrieving aggregation history ...';
+export const loadingAggregationHistoryError = 'System error retrieving repository aggregation history.';
+export const noAggregationHistory = 'There is no aggregation history for this repository at the moment';
+
+/* Interfaces Messages */
+export const noInterfacesSaved = 'You have to create at least one interface';
+export const nonRemovableInterface = 'This interface can not be removed!';
+
+/* Rules Messages */
+export const loadingRuleSets = 'Retrieving sets of rules ...';
+export const loadingRuleSetsError = 'The rule sets can not be retrieved at the moment';
+export const noRuleSets = 'No rule sets were found';
+export const noContentRulesResults = 'No content rules were tested';
+export const noUsageRulesResults = 'No usage rules were tested';
+export const didntSelectRules = 'You need to select at least one rule';
+export const didntSelectCrisEntities = 'You need to select at least one entity';
+
+/* Loading Validation Sets Messages */
+export const loadingValSets = 'Retrieving validation sets ...';
+export const loadingValSetsError = 'The validation sets can not be retrieved at the moment';
+
+/* Jobs of User */
+export const loadingUserJobs = 'Retrieving job results ...';
+export const loadingUserJobsError = 'Your job results can not be retrieved at the moment';
+export const noUserJobsFound = 'No stored jobs found';
+export const loadingJobSummary = 'Retrieving job summary ...';
+export const loadingJobSummaryError = 'The job summary can not be retrieved at the moment';
+export const submittingJob = 'Submitting the new job';
+export const submittingJobError = 'The job could not be submitted';
+
+
+/* Loading Topics Messages */
+export const loadingTopics = 'Retrieving topics for the datasource...';
+export const loadingTopicsError = 'Failed to load the topics for your datasource';
+export const noTopicsFound = 'No topics were found for this datasource';
+
+/* Loading Events Messages */
+export const loadingEvents = 'Retrieving events for topic ...';
+export const loadingEventsError = 'Failed to retrieve events';
+export const noEventsForTopic = 'No events were found for this topic';
+export const noEventsWithParams = 'No events were found with the given parameters';
+
+/* Loading Subscriptions Messages */
+export const loadingSubscriptions = 'Retrieving subscriptions ...';
+export const noSubscriptionsFound = 'No subscriptions found for your account';
+export const deletingSubscription = 'Deleting subscription ...';
+export const deletingSubscriptionError = 'Failed to delete your subscription';
+export const deletingSubscriptionSuccess = 'The subscription was deleted successfully';
+export const subscribingToEvents = 'Subscribing to events ...';
+export const subscribingToEventsError = 'Failed to subscribe you to the chosen events';
+export const subscribingToeventsSuccess = 'The subscription was submitted successfully';
+export const subscribingChooseFrequency = 'Please choose a frequency for the notifications';
+
+/* Piwik Messages */
+export const loadingMetrics = 'Loading diagrams ...';
+export const loadingMetricsError = 'Failed to load the metrics for this repository';
+export const enablingMetrics = 'Enabling metrics for this repository...';
+export const enabledMetricsSuccess = 'The metrics for this repository were enabled';
+export const enabledMetricsError = 'System error enabling metrics for this repository.';
+
+
+/* Forms validation Messages */
+export const formInfoLoading = 'Loading information';
+export const formInterfacesLoading = 'Loading interfaces';
+export const formSubmitting = 'Saving changes ...';
+export const formErrorRequiredFields = 'All the fields marked with (*) are mandatory';
+export const formErrorInvalidFields = 'There are invalid fields';
+export const formErrorWasntSaved = 'The form could not be submitted';
+export const formSuccessUpdatedRepo = 'The datasource was updated successfully.';
+export const formSuccessRegisteredDatasource = 'The datasource was registered successfully';
+export const formSuccessAddedInterface = 'The interface was added successfully.';
+export const formSuccessUpdatedInterface = 'The interface was updated successfully.';
+
+/* BaseUrl validation Messages */
+export const didntChooseBaseUrl = 'You need to select a base URL';
+export const invalidCustomBaseUrl = 'The url you entered is not valid';
+export const identifyingUrl = 'Identifying Url ...';
---
+++ dnet45/modules/uoa-repository-dashboard-gui/branches/angular-6/src/app/services/repository.service.ts
@@ -0,0 +1,159 @@
+/*
+* Created by myrto on 12/05/2017
+*/
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import {
+  AggregationDetails,
+  Country, MetricsInfo,
+  Repository,
+  RepositoryInterface,
+  RepositorySnippet,
+  Timezone,
+  Typology
+} from '../domain/typeScriptClasses';
+import { Observable, of } from 'rxjs';
+import { timezones } from '../domain/timezones';
+import { typologies } from '../domain/typologies';
+
+const headerOptions = {
+  headers : new HttpHeaders().set('Content-Type', 'application/json')
+                             .set('Accept', 'application/json'),
+  withCredentials: true
+};
+
+
+@Injectable ()
+export class RepositoryService {
+  private apiUrl = environment.API_ENDPOINT + '/repository/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  addInterface(datatype: string, repoId: string, registeredBy: string, newInterface: RepositoryInterface): Observable<RepositoryInterface> {
+    const url = `${this.apiUrl}addInterface?datatype=${datatype}&repoId=${repoId}&registeredBy=${registeredBy}`;
+    console.log(`knocking on: ${url}`);
+    console.log(`sending ${JSON.stringify(newInterface)}`);
+    return this.httpClient.post<RepositoryInterface>(url, newInterface, headerOptions);
+  }
+
+  updateInterface(repoId: string, registeredBy: string, interfaceInfo: RepositoryInterface): Observable<RepositoryInterface> {
+    const url = `${this.apiUrl}updateRepositoryInterface?repoId=${repoId}&registeredBy=${registeredBy}`;
+    console.log(`knocking on: ${url}`);
+    console.log(`sending ${JSON.stringify(interfaceInfo)}`);
+    return this.httpClient.post<RepositoryInterface>(url, interfaceInfo, headerOptions);
+  }
+
+  deleteInterface(id: string, registeredBy: string) {
+    const url = `${this.apiUrl}deleteInterface/?id=${id}&registeredBy=${registeredBy}`;
+    console.log(`knocking on: ${url}`);
+
+    return this.httpClient.delete(url, {withCredentials: true, responseType: 'text'});
+  }
+
+  addRepository(datatype: string, newRepository: Repository): Observable<Repository> {
+    const url = `${this.apiUrl}addRepository?datatype=${datatype}`;
+    console.log(`knocking on: ${url}`);
+    console.log(`sending ${JSON.stringify(newRepository)}`);
+    return this.httpClient.post<Repository>(url, newRepository, headerOptions);
+  }
+
+  updateRepository(repoInfo: Repository): Observable<Repository> {
+    const url = `${this.apiUrl}updateRepository`;
+    console.log(`knocking on: ${url}`);
+    console.log(`sending ${JSON.stringify(repoInfo)}`);
+    return this.httpClient.post<Repository>(url, repoInfo, headerOptions);
+  }
+
+  getRepositoriesOfCountry(country: string, mode: string): Observable<RepositorySnippet[]> {
+    const url = `${this.apiUrl}getRepositoriesByCountry/${country}/${mode}`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<RepositorySnippet[]>(url, headerOptions);
+  }
+
+  getRepositoriesOfUser(userEmail: string): Observable<Repository[]> {
+    const url = `${this.apiUrl}getRepositoriesOfUser/${userEmail}/0/100`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<Repository[]>(url, headerOptions);
+  }
+
+
+  getRepositoryById(id: string): Observable<Repository> {
+    const url = `${this.apiUrl}getRepositoryById/${id}`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<Repository>(url, headerOptions);
+  }
+
+  getRepositoryInterface(id: string): Observable<RepositoryInterface[]> {
+    const url = `${this.apiUrl}getRepositoryInterface/${id}`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<RepositoryInterface[]>(url, headerOptions);
+  }
+
+
+  getUrlsOfUserRepos(userEmail: string): Observable<string[]> {
+    const url = `${this.apiUrl}getUrlsOfUserRepos/${userEmail}/0/100/`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<string[]>(url, headerOptions);
+  }
+
+  getRepositoryAggregations(id: string): Observable<AggregationDetails[]> {
+    const url = `${this.apiUrl}getRepositoryAggregations/${id}`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<AggregationDetails[]>(url, headerOptions);
+  }
+
+  getRepositoryAggregationsByYear(id: string): Observable<Map<string, AggregationDetails[]>> {
+    const url = `${this.apiUrl}getRepositoryAggregationsByYear/${id}`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<Map<string, AggregationDetails[]>>(url, headerOptions);
+  }
+
+  getTimezones(): Observable<Timezone[]> {
+/*    const url = `${this.apiUrl}getTimezones`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<Timezone[]>(url, headerOptions);*/
+    return of(<Timezone[]>timezones);
+  }
+
+  getTypologies(): Observable<Typology[]> {
+/*    const url = `${this.apiUrl}getTypologies`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<Typology[]>(url, headerOptions);*/
+    return of(<Typology[]>typologies);
+  }
+
+  getCountries(): Observable<Country[]> {
+    const url = `${this.apiUrl}getCountries`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<Country[]>(url, headerOptions);
+  }
+
+
+  getCompatibilityClasses (mode: string): Observable<Map<string, string>> {
+    const url = `${this.apiUrl}getCompatibilityClasses/${mode}`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<Map<string, string>>(url, headerOptions);
+  }
+
+  getDatasourceClasses(mode: string): Observable<Map<string, string>> {
+    const url = `${this.apiUrl}getDatasourceClasses/${mode}`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<Map<string, string>>(url, headerOptions);
+  }
+
+
+  getMetricsInfoForRepository (repoId: string): Observable<MetricsInfo> {
+    const url = `${this.apiUrl}getMetricsInfoForRepository/${repoId}`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<MetricsInfo>(url, headerOptions);
+  }
+
+  getListLatestUpdate(mode: string): Observable<any> {
+    const url = `${this.apiUrl}getListLatestUpdate/${mode}`;
+    console.log(`knocking on: ${url}`);
+    return this.httpClient.get<any>(url, headerOptions);
+  }
+
+}
