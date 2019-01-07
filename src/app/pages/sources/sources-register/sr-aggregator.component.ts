d0ab0e43ce7f3c1619a1a5e6744dc7d53e556763
myrto.koukouli <myrto.koukouli@d315682c-612b-4755-9ff5-7f18f6832af3>
2019-01-07 11:10:04 +0000

    moving angular 6 project to trunk

---
+++ src/app/pages/sources/sources-register/sr-aggregator.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Repository, RepositoryInterface } from '../../../domain/typeScriptClasses';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AsideHelpContentComponent, HelpContentComponent } from '../../../shared/reusablecomponents/help-content.component';
+import { DatasourceCreateFormComponent } from '../sources-forms/datasource-create-form.component';
+import { RepositoryService } from '../../../services/repository.service';
+import { noInterfacesSaved } from '../../../domain/shared-messages';
+import { DatasourceNewInterfaceFormComponent } from '../sources-forms/datasource-new-interface-form.component';
+import { concatMap } from 'rxjs/operators';
+import { from, of } from 'rxjs';
+import { RegisterNewDatasourceComponent } from './register-new-datasource.component';
+
+@Component ({
+  selector: 'sr-aggregator',
+  templateUrl: './register-new-datasource.component.html'
+})
+export class SrAggregatorComponent extends RegisterNewDatasourceComponent implements OnInit {
+
+  ngOnInit() {
+    this.datasourceType = 'aggregator';
+    super.ngOnInit();
+  }
+
+}
