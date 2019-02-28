4b551f0531cf6a2afab326ced619cabbc874484c
panagiotis.kanakakis <panagiotis.kanakakis@d315682c-612b-4755-9ff5-7f18f6832af3>
2019-02-28 14:14:56 +0000

    Fix on enviroment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: 'https://beta.provide.openaire.eu/api',
-  FAQ_ENDPOINT: 'https://provide.openaire.eu/uoa-admin-tools/api',
-  FAQ_HOMEPAGE: 'https://provide.openaire.eu/uoa-admin-tools/index.html',
-  AAI_ENDPOINT: 'https://beta.provide.openaire.eu/api/openid_connect_login'
+  API_ENDPOINT: '/uoa-repository-manager-service',
+  FAQ_ENDPOINT: '/api',
+  FAQ_HOMEPAGE: '/uoa-admin-tools/'
 };
