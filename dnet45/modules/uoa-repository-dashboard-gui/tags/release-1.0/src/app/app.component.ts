98c2244cc86fda56f1fd651ef30e2ad619de63a0
panagiotis.kanakakis <panagiotis.kanakakis@d315682c-612b-4755-9ff5-7f18f6832af3>
2019-02-28 14:18:01 +0000

    Release 1.0 of uoa-repository-manager dashboard

---
+++ dnet45/modules/uoa-repository-dashboard-gui/tags/release-1.0/src/app/app.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RoutesRecognized } from '@angular/router';
+import { AuthenticationService } from './services/authentication.service';
+import { environment } from '../environments/environment';
+import { MatomoInjector } from 'ngx-matomo';
+
+@Component({
+  selector: 'oa-repo-manager',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  constructor(private router: Router,
+              private authService: AuthenticationService,
+              private matomoInjector: MatomoInjector) {
+
+    console.log('11-02-2019. First deploy of project upgraded to angular 6');
+
+    let piwikUrl;
+    if (window.location.origin.includes('beta')) {
+      // piwikUrl = 'https://analytics.openaire.eu/piwik.php?idsite=92&rec=1';
+      piwikUrl = '92';
+    } else if (window.location.origin.includes('localhost:4200') ||
+               window.location.origin.includes('athenarc')) {
+      // piwikUrl = 'https://analytics.openaire.eu/piwik.php?idsite=92&rec=1';
+      piwikUrl = '9222222';
+    } else {
+      // piwikUrl = 'https://analytics.openaire.eu/piwik.php?idsite=111&rec=1';
+      piwikUrl = '111';
+    }
+    this.matomoInjector.init('https://analytics.openaire.eu/', piwikUrl);
+
+    /*disabling console.log in production*/
+    if ( environment.production === true ) {
+      console.log = function () {};
+    }
+
+    // URL of the SPA to redirect the user to after login
+    // this.authService.redirectUrl = "/dashboard";
+
+    if (window.location.pathname.includes('/compatibility/browseHistory/')) {
+      this.authService.redirectUrl = window.location.pathname;
+      console.log('redirectUrl', this.authService.redirectUrl);
+    }
+
+    this.authService.tryLogin();
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      /*if ((evt instanceof RoutesRecognized) && (environment.production === true)) {
+        let piwikUrl;
+        if (window.location.origin.includes('beta')) {
+          // piwikUrl = 'https://analytics.openaire.eu/piwik.php?idsite=92&rec=1';
+          piwikUrl = '92';
+        } else if (window.location.origin.includes('localhost:4200') ||
+                   window.location.origin.includes('athenarc')) {
+          // piwikUrl = 'https://analytics.openaire.eu/piwik.php?idsite=92&rec=1';
+          piwikUrl = '9222222';
+        } else {
+          // piwikUrl = 'https://analytics.openaire.eu/piwik.php?idsite=111&rec=1';
+          piwikUrl = '111';
+        }
+        this.matomoInjector.init('https://analytics.openaire.eu/', piwikUrl);
+      }*/
+
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
+  }
+
+}
---
+++ dnet45/modules/uoa-repository-dashboard-gui/tags/release-1.0/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  API_ENDPOINT: '/uoa-repository-manager-service',
+  FAQ_ENDPOINT: '/api',
+  FAQ_HOMEPAGE: '/uoa-admin-tools/'
+};
