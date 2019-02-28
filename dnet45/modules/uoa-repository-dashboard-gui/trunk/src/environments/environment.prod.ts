84873b03703fbffcb566e19ef5fd9f37f14b4ba9
myrto.koukouli <myrto.koukouli@d315682c-612b-4755-9ff5-7f18f6832af3>
2019-02-28 15:36:44 +0000

    updated production environment variables

--- dnet45/modules/uoa-repository-dashboard-gui/trunk/src/environments/environment.prod.ts
+++ dnet45/modules/uoa-repository-dashboard-gui/trunk/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_ENDPOINT: '/uoa-repository-manager-service',
-  FAQ_ENDPOINT: '/api',
-  FAQ_HOMEPAGE: '/uoa-admin-tools/'
+  FAQ_ENDPOINT: '/uoa-admin-tools/api',
+  FAQ_HOMEPAGE: '/uoa-admin-tools/dashboard'
 };
