6211384ea8d2565bf6046b434e3fda18eeb1a8c3
myrto.koukouli <myrto.koukouli@d315682c-612b-4755-9ff5-7f18f6832af3>
2019-03-20 14:45:37 +0000

    2nd attempt to fix utf-8 issue

--- dnet45/modules/uoa-repository-dashboard-gui/trunk/src/app/pages/sources/sources-forms/datasource-create-form.component.ts
+++ dnet45/modules/uoa-repository-dashboard-gui/trunk/src/app/pages/sources/sources-forms/datasource-create-form.component.ts
@@ -208,20 +208,20 @@ export class DatasourceCreateFormComponent implements OnInit {
 
   createNewRepository(): Repository {
     const newRepo: Repository = new Repository();
-    newRepo.officialName = encodeURI(this.group.get('officialName').value);
-    newRepo.englishName = encodeURI(this.group.get('englishName').value);
+    newRepo.officialName = this.group.get('officialName').value.toString();
+    newRepo.englishName = this.group.get('englishName').value.toString();
     newRepo.websiteUrl = this.group.get('websiteUrl').value;
     newRepo.logoUrl = this.group.get('logoUrl').value;
     newRepo.contactEmail = this.group.get('adminEmail').value;
     newRepo.countryName = this.countries.filter(x => x.code === this.group.get('country').value)[0].name;
     newRepo.countryCode = this.group.get('country').value;
-    newRepo.organization = encodeURI(this.group.get('institutionName').value);
+    newRepo.organization = this.group.get('institutionName').value.toString();
     newRepo.latitude = this.group.get('latitude').value;
     newRepo.longitude = this.group.get('longtitude').value;
     newRepo.timezone = this.group.get('timezone').value;
     newRepo.datasourceClass = this.group.get('datasourceType').value;
     newRepo.typology = this.group.get('softwarePlatform').value;
-    newRepo.description = encodeURI(this.group.get('repoDescription').value);
+    newRepo.description = this.group.get('repoDescription').value.toString();
     newRepo.issn = '';
     newRepo.eissn = '';
     newRepo.lissn = '';
