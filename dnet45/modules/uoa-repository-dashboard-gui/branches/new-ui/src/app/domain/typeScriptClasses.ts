7efd79179d8b2be0ce47504d8c57ad25b5b111d3
andreas.mantas <andreas.mantas@d315682c-612b-4755-9ff5-7f18f6832af3>
2020-01-20 14:38:17 +0000

    added software field to events list

--- dnet45/modules/uoa-repository-dashboard-gui/branches/new-ui/src/app/domain/typeScriptClasses.ts
+++ dnet45/modules/uoa-repository-dashboard-gui/branches/new-ui/src/app/domain/typeScriptClasses.ts
@@ -484,6 +484,7 @@ export class Publication implements IsSerializable {
     publications: Publication[];
     projects: Project[];
     datasets: Dataset[];
+    softwares: Software[];
 }
 
 export class Range implements IsSerializable {
@@ -498,6 +499,13 @@ export class SimpleSubscriptionDesc implements IsSerializable {
     count: number;
     creationDate: Date;
     lastNotificationDate: Date;
+}
+
+export class Software implements IsSerializable {
+  name: string;
+  description: string;
+  landingPage: string;
+  repository: string;
 }
 
 export class Subscription implements IsSerializable {
