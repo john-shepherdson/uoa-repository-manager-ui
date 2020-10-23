65c64f5a832c25ff8f7a27c0067e92c5c3f87f2e
stefania.martziou <stefania.martziou@d315682c-612b-4755-9ff5-7f18f6832af3>
2020-10-23 09:54:24 +0000

    Created a new environment properties file for beta

--- dnet45/modules/uoa-repository-dashboard-gui/branches/new-ui/src/environments/environment.ts
+++ dnet45/modules/uoa-repository-dashboard-gui/branches/new-ui/src/environments/environment.ts
@@ -5,8 +5,10 @@
 export const environment = {
   production: false,
   API_ENDPOINT: '/uoa-repository-manager-service',
-  FAQ_ENDPOINT: 'http://88.197.53.69:5555/api',
-  FAQ_HOMEPAGE: 'http://88.197.53.69:5555'
+  // FAQ_ENDPOINT: 'http://88.197.53.69:5555/api',
+  // FAQ_HOMEPAGE: 'http://88.197.53.69:5555'
+  FAQ_ENDPOINT: 'https://provide.openaire.eu/uoa-admin-tools/api',
+  FAQ_HOMEPAGE: 'https://provide.openaire.eu/uoa-admin-tools/dashboard'
 };
 
 /*
