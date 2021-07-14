6c8705d215e3307ca35ce2d7a315adf13507f165
stefania.martziou <stefania.martziou@d315682c-612b-4755-9ff5-7f18f6832af3>
2021-07-14 14:21:38 +0000

    Branching to upgrade angular

---
+++ dnet45/modules/uoa-repository-dashboard-gui/branches/angular-upgrade/src/app/pages/compatibility/compatibility-validate.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component ({
+  selector: 'app-compatibility-validation',
+  templateUrl: 'compatibility-validate.component.html'
+})
+
+export class CompatibilityValidateComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+    let body = document.getElementsByTagName('body')[0];
+    body.classList.remove("top_bar_active");   //remove the class
+    body.classList.remove("page_heading_active");
+    body.classList.remove("landing");
+    body.classList.add("dashboard");
+  }
+}
---
+++ dnet45/modules/uoa-repository-dashboard-gui/branches/angular-upgrade/src/app/pages/emptypage/empty-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-empty-page',
+  templateUrl: './empty-page.component.html',
+})
+
+export class EmptyPageComponent {
+
+  constructor() {}
+}
