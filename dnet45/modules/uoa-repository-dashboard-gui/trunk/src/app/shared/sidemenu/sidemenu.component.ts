be0a0b84e8a5413c8ea9d8dd689a0e577a6bab08
spyroukon <spyroukon@d315682c-612b-4755-9ff5-7f18f6832af3>
2021-07-16 11:47:34 +0000

    changed roles for super admin and provide admin

--- dnet45/modules/uoa-repository-dashboard-gui/trunk/src/app/shared/sidemenu/sidemenu.component.ts
+++ dnet45/modules/uoa-repository-dashboard-gui/trunk/src/app/shared/sidemenu/sidemenu.component.ts
@@ -136,8 +136,8 @@ export class SideMenuComponent implements OnInit {
   }
 
   getIsUserAdmin() {
-    this.isUserAdmin = (this.authService.getUserRole().includes('ROLE_ADMIN') ||
-      this.authService.getUserRole().includes('ROLE_PROVIDE_ADMIN'));
+    this.isUserAdmin = (this.authService.getUserRole().includes('SUPER_ADMINISTRATOR') ||
+      this.authService.getUserRole().includes('CONTENT_PROVIDER_DASHBOARD_ADMINISTRATOR'));
     return this.isUserAdmin;
   }
 
