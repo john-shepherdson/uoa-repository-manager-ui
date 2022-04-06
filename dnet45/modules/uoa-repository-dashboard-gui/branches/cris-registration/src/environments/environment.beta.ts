1cc2cabacc2a0e66c3efadcb3ce9482c17b98b3a
stefania.martziou <stefania.martziou@d315682c-612b-4755-9ff5-7f18f6832af3>
2022-04-06 10:13:29 +0000

    Deleting old cris registration branch which was never used.

--- dnet45/modules/uoa-repository-dashboard-gui/branches/cris-registration/src/environments/environment.beta.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  AAI_LOGOUT: 'https://aai.openaire.eu/proxy/saml2/idp/SingleLogoutService.php?ReturnTo=',
-  MATOMO_URL: 'https://analytics.openaire.eu/',
-  MATOMO_SITE: 92,
-  API_ENDPOINT: '/api',
-  FAQ_ENDPOINT: 'https://provide.openaire.eu/uoa-admin-tools/api',
-  FAQ_HOMEPAGE: 'https://provide.openaire.eu/uoa-admin-tools/dashboard'
-};
--- dnet45/modules/uoa-repository-dashboard-gui/branches/cris-registration/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  AAI_LOGOUT: 'https://aai.openaire.eu/proxy/saml2/idp/SingleLogoutService.php?ReturnTo=',
-  MATOMO_URL: 'https://analytics.openaire.eu/',
-  MATOMO_SITE: 111,
-  API_ENDPOINT: '/api',
-  FAQ_ENDPOINT: '/uoa-admin-tools/api',
-  FAQ_HOMEPAGE: '/uoa-admin-tools/dashboard'
-};
