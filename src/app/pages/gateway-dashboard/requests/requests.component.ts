0666def3f7313340f6047baf3ad6153f9f449fb4
Vasilis <billnick@hotmail.gr>
2025-09-16 10:50:57 +0300

    feat(requests): add table and API calls to fetch request data

--- src/app/pages/gateway-dashboard/requests/requests.component.ts
+++ src/app/pages/gateway-dashboard/requests/requests.component.ts
@@ -1,9 +1,61 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RequestsService } from "../services/requests.service";
 
 @Component({
   selector: 'gateway-requests',
   standalone: true,
   templateUrl: './requests.component.html',
+  imports: [CommonModule]
 })
 
-export class RequestsComponent {}
+export class RequestsComponent {
+  requests: any[] = [];
+
+  constructor(private requestsService: RequestsService) {}
+
+  ngOnInit(): void {
+    this.requestsService.getRequests({
+      page: 0,
+      size: 10,
+      sort: 'status',
+      order: 'ASC',
+    }).subscribe(data => {
+      console.log(data);
+      this.requests = data.results;
+    })
+  }
+//   request = {
+//   "id": 0,
+//   "requestType": "PRIMARY",
+//   "datasourceId": "string",
+//   "sourceGatewayId": "string",
+//   "targetGatewayId": "string",
+//   "status": "PENDING",
+//   "ownerApproval": {
+//     "actorId": "string",
+//     "decision": "APPROVED",
+//     "decidedAt": "2025-09-15T08:12:19.231Z",
+//     "comment": "string",
+//     "approved": true
+//   },
+//   "sourceGatewayApproval": {
+//     "actorId": "string",
+//     "decision": "APPROVED",
+//     "decidedAt": "2025-09-15T08:12:19.231Z",
+//     "comment": "string",
+//      "approved": true
+//   },
+//   "targetGatewayApproval": {
+//     "actorId": "string",
+//     "decision": "APPROVED",
+//     "decidedAt": "2025-09-15T08:12:19.231Z",
+//     "comment": "string",
+//     "approved": true
+//   },
+//   "createdAt": "2025-09-15T08:12:19.231Z",
+//   "createdBy": "string",
+//   "updatedAt": "2025-09-15T08:12:19.231Z",
+//   "updatedBy": "string"
+// }
+}
