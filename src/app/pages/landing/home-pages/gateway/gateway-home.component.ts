419c76f60e335890ed1153c96ef3c8695f3bab82
Konstantinos Spyrou <spyroukon@gmail.com>
2025-10-09 14:22:22 +0300

    fix: corrects update request method

--- src/app/pages/landing/home-pages/gateway/gateway-home.component.ts
+++ src/app/pages/landing/home-pages/gateway/gateway-home.component.ts
@@ -1,17 +1,67 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { CommunityContextService } from 'src/app/services/communityContext.service';
-import { Observable } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule, DecimalPipe} from '@angular/common';
+import {UsageStatsSummary} from '../../../../domain/typeScriptClasses';
+import {AuthenticationService} from '../../../../services/authentication.service';
+import {StatisticsService} from '../../../../services/statistics.service';
+import {Router, RouterModule} from '@angular/router';
+
 @Component({
-    selector: 'app-gateway-home',
-    templateUrl: './gateway-home.component.html',
-    standalone: true
+  selector: 'app-gateway-home',
+  templateUrl: './gateway-home.component.html',
+  imports: [
+    DecimalPipe,
+    CommonModule,
+    RouterModule,
+  ],
+  standalone: true
 })
 
-export class GatewayHomeComponent  {
-    currentCommunityId$: Observable<string | null> = null;
+export class GatewayHomeComponent implements OnInit {
+  statisticsNumbers: UsageStatsSummary;
+  inBeta: boolean;
 
-    constructor(private communityContextService: CommunityContextService) {}
 
-    
+  constructor(private authService: AuthenticationService,
+              private statsService: StatisticsService,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    this.getStatisticsNumbers();
+
+    const baseUrl = window.location.origin;
+    this.inBeta = (baseUrl.includes('beta') || baseUrl.includes('athenarc'));
+
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('dashboard');
+    body.classList.add('landing');
+  }
+
+
+  login() {
+    this.authService.loginWithState();
+  }
+
+  getStatisticsNumbers() {
+    this.statsService.getStatisticsNumbers().subscribe(
+      res => {
+        this.statisticsNumbers = res;
+        // this.statisticsNumbers.lastYearUsagestats = JSON.parse(res['lastYearUsagestats'].toString());
+      },
+      error => console.debug(error.message),
+      () => {
+        // console.log('statisticsNumbers is', JSON.stringify(this.statisticsNumbers));
+      }
+    );
+  }
+
+  goToPage(pageUrl: string) {
+    if (this.authService.getIsUserLoggedIn()) {
+      this.router.navigate([pageUrl]);
+    } else {
+      // this.authService.redirectUrl = pageUrl;
+      this.login();
+    }
+  }
 }
 
